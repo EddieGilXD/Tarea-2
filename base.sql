05f2bb98a3665d5ff3e874825d8a8866a2357043
EddieGilXD <djuntaied@gmail.com>
2024-12-09 22:33:44 -0600

    Tarea terminada, .gitignore agregado no agregue gitignore dentro del archivo para que se viera que ignore la carpeta

--- base.sql
+++ base.sql
@@ -1,10 +1,17 @@
 CREATE DATABASE IF NOT EXISTS BancoDB;
-USE BancoDB
 
-CREATE TABLE Clientes (
+CREATE TABLE IF NOT EXIST Clientes (
     id_cliente INT PRIMARY KEY,
     nombre VARCHAR(50),
     ciudad VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXIST Cuentas (
+    id_cuenta INT PRIMARY KEY,
+    id_cliente INT,
+    tipo_cuenta VARCHAR(50),
+    saldo DECIMAL (10,2),
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente)
 );
 
 INSERT INTO Clientes (id_cliente, nombre, ciudad) VALUES
@@ -14,17 +21,9 @@ INSERT INTO Clientes (id_cliente, nombre, ciudad) VALUES
 (4,  'Ana', 'Solola'),
 (5, 'Ricardo', 'Reu');
 
-CREATE TABLE Cuentas (
-    id_cuenta INT PRIMARY KEY,
-    id_cliente INT,
-    tipo_cuenta VARCHAR(50),
-    saldo DECIMAL (10,2),
-    FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente)
-);
-
 INSERT INTO Cuentas (id_cuenta, id_cliente, tipo_cuenta, saldo) VALUES
 (10, 1, 'Monetaria', 1000.00),
-(20, 1, 'Ahorro', 300.00),
+(20, 2, 'Ahorro', 300.00),
 (30, 3, 'Monetaria', 900.00),
 (40, 4, 'Monetaria', 90.51),
 (50, 4, 'Monetaria', 750.00);
