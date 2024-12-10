7282cc24529e49c7e0cf3f07350e9a7a3fe9dac0
EddieGilXD <djuntaied@gmail.com>
2024-12-09 19:54:32 -0600

    terminando joins

--- base.sql
+++ base.sql
@@ -24,7 +24,7 @@ CREATE TABLE Cuentas (
 
 INSERT INTO Cuentas (id_cuenta, id_cliente, tipo_cuenta, saldo) VALUES
 (10, 1, 'Monetaria', 1000.00),
-(20, 2, 'Ahorro', 300.00),
+(20, 1, 'Ahorro', 300.00),
 (30, 3, 'Monetaria', 900.00),
 (40, 4, 'Monetaria', 90.51),
 (50, 4, 'Monetaria', 750.00);
--- servidor.js
+++ servidor.js
@@ -2,15 +2,34 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',    
+    user: 'root',
+    password: "Windows@32",
     database: 'BancoDB'
 })
 
-connection.query('SELECT * FROM Clientes',(err, results, fields)=>{
+const closeConnection = () => {
+    connection.end((err)=>{
+        if (err){
+            console.log("Error al cerrar la conexion: ", err)
+        }else{
+            console.log("Conexion cerrada")
+        }
+    })
+}
+
+connection.query('SELECT * FROM Cuentas',(err, results, fields)=>{
     if (err){
-        console.log("Error en query", err)
+        console.log("Error en query: ", err)
     } else{
-        console.log("Resultados: ", results)
-        console.log("Campos: ", fields)
-    }
+        console.log("Listado de todas las cuentas creadas: ", results)        
+    }    
+})
+
+connection.query('SELECT * FROM Clientes',(err, results, fields) =>{
+    if(err){
+        console.log("Error en query: ", err)
+    } else {
+        console.log("Listado de todos los clientes creados: ", results)                
+    }    
+    closeConnection();
 })
